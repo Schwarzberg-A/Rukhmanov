7a21ba2b6683882be7b3498d125f1931885ed274
Aleks <aleksei.rukhmanov@gmx.de>
2021-01-22 03:35:04 +0300

    some animation added

--- src/js/script.js
+++ src/js/script.js
@@ -7,12 +7,14 @@ const nav = document.querySelector('.header__nav');
 const persdata = document.querySelector('.header-wrapper');
 const arrow = document.querySelector('.arrow');
 burger.addEventListener('click', () => {
-  nav.classList.toggle('menu-open');
-  persdata.classList.toggle('menu-close');
+  nav.classList.toggle('menu');
+
+  // nav.classList.toggle('menu-open');
+  // nav.classList.toggle('menu-close');
   burger.classList.toggle('open');
 })
 
-//=============== skills ==========================-------->
+//=============== foto ==========================-------->
 const foto = document.querySelector('.main-foto__img');
 foto.addEventListener('click', () => {
   foto.classList.toggle('full');
@@ -30,6 +32,8 @@ function getCoordsEnd (elem) {
   return box.bottom + pageYOffset
 }
 
+let extraPX = 35;
+
 window.addEventListener('scroll', () => {
   const skillListYCoordsStart = getCoordsStart (skillList); 
   const skillListYCoordsEnd = getCoordsEnd (skillList); 
@@ -44,10 +48,12 @@ window.addEventListener('scroll', () => {
   for (let i = 0; i < skillItems.length; i++) {
     let skillItemYCoordsStart = getCoordsStart(skillItems[i]);
     let skillItemYCoordsEnd = getCoordsEnd(skillItems[i]);
-    if ((scroll > skillItemYCoordsStart) && ((scroll < skillItemYCoordsEnd))) {
-      skillItems[i].classList.add('toLeft');
+    if ((scroll > (skillItemYCoordsStart - extraPX)) && ((scroll < (skillItemYCoordsEnd + extraPX)))) {
+      skillItems[i].querySelector('span').classList.add('visi');
+      skillItems[i].querySelector('img').classList.add('toleft');
     } else {
-      skillItems[i].classList.remove('toLeft');
+      skillItems[i].querySelector('span').classList.remove('visi');
+      skillItems[i].querySelector('img').classList.remove('toleft');
     }
   }
 
